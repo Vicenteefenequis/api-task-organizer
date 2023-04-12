3f09ab74d8a6de4e4721d92c0179c4cc736b5523
Vicente Junior <vicente19981@live.com>
2023-04-11 22:36:13 -0300

    feat: create identifier task id

---
+++ domain/src/main/java/com/vicente/task/organizer/task/TaskID.java
@@ -0,0 +1,41 @@
+package com.vicente.task.organizer.task;
+
+import com.vicente.task.organizer.Identifier;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class TaskID extends Identifier {
+    private final String value;
+
+    public TaskID(String value) {
+        Objects.requireNonNull(value);
+        this.value = value;
+    }
+
+    public static TaskID unique() {
+        return TaskID.from(UUID.randomUUID());
+    }
+
+    public static TaskID from(final UUID anId) {
+        return new TaskID(anId.toString().toLowerCase());
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final TaskID that = (TaskID) o;
+        return Objects.equals(getValue(), that.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
+    }
+}
