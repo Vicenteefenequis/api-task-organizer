f183a27d4ad726ae65695a1a4d289fa7ef89d885
Vicente Junior <vicente19981@live.com>
2023-04-13 17:16:51 -0300

    feat: create default get task by id usecase

---
+++ application/src/main/java/com/vicente/task/organizer/task/retrieve/get/GetTaskByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.vicente.task.organizer.task.retrieve.get;
+
+import com.vicente.task.organizer.UseCase;
+
+public abstract class GetTaskByIdUseCase extends UseCase<String, TaskOutput> {
+}
---
+++ application/src/main/java/com/vicente/task/organizer/task/retrieve/get/TaskOutput.java
@@ -0,0 +1,29 @@
+package com.vicente.task.organizer.task.retrieve.get;
+
+import com.vicente.task.organizer.task.Task;
+
+import java.time.Instant;
+
+public record TaskOutput(
+        String id,
+        String name,
+        String description,
+        boolean isCompleted,
+        Instant dueDateAt,
+        Instant createdAt,
+        Instant updatedAt,
+        Instant deletedAt
+) {
+    public static TaskOutput with(final Task aTask) {
+        return new TaskOutput(
+                aTask.getId().getValue(),
+                aTask.getName(),
+                aTask.getDescription(),
+                aTask.isCompleted(),
+                aTask.getDueDateAt(),
+                aTask.getCreatedAt(),
+                aTask.getUpdatedAt(),
+                aTask.getDeletedAt()
+        );
+    }
+}
