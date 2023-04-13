afcdb570da3e0072568ddea925b90863a6dea7f8
Vicente Junior <vicente19981@live.com>
2023-04-12 22:28:11 -0300

    feat: create task command and output

---
+++ application/src/main/java/com/vicente/task/organizer/task/create/CreateTaskCommand.java
@@ -0,0 +1,14 @@
+package com.vicente.task.organizer.task.create;
+
+import java.time.Instant;
+
+public record CreateTaskCommand(
+        String name,
+        String description,
+        boolean isCompleted,
+        Instant dueDate
+) {
+    public static CreateTaskCommand with(final String aName, final String aDescription, final boolean isCompleted, final Instant aDueDate) {
+        return new CreateTaskCommand(aName, aDescription, isCompleted, aDueDate);
+    }
+}
