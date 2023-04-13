fe3de6765d79bdf8e660ce17c2da704d723fa1f2
Vicente Junior <vicente19981@live.com>
2023-04-13 17:55:22 -0300

    test: ensure ListTaskUseCase call list task then return tasks

---
+++ application/src/test/java/com/vicente/task/organizer/task/retrieve/list/ListTaskUseCaseTest.java
@@ -0,0 +1,84 @@
+package com.vicente.task.organizer.task.retrieve.list;
+
+import com.vicente.task.organizer.pagination.Pagination;
+import com.vicente.task.organizer.pagination.SearchQuery;
+import com.vicente.task.organizer.task.Task;
+import com.vicente.task.organizer.task.TaskGateway;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public class ListTaskUseCaseTest {
+    @InjectMocks
+    private DefaultListTasksUseCase listTaskUseCase;
+    @Mock
+    private TaskGateway taskGateway;
+
+    @BeforeEach
+    void cleanUp() {
+        Mockito.reset(taskGateway);
+    }
+
+
+    @Test
+    public void givenAValidCommandQuery_whenCallListTask_thenReturnTaskList() {
+        // given
+        final var expectedPage = 1;
+        final var expectedPerPage = 2;
+        final var expectedTerms = "";
+        final var expectedSort = "name";
+        final var expectedDirection = "desc";
+
+        final var tasks = List.of(
+                Task.newTask(
+                        "Task 1",
+                        "Description 1",
+                        false,
+                        Instant.parse("2021-01-01T00:00:00Z")
+                ),
+                Task.newTask(
+                        "Task 2",
+                        "Description 2",
+                        false,
+                        Instant.parse("2021-01-01T00:00:00Z")
+                )
+        );
+
+        final var aQuery = SearchQuery.with(
+                expectedPage,
+                expectedPerPage,
+                expectedTerms,
+                expectedSort,
+                expectedDirection
+        );
+
+        final var expectedPagination = new Pagination<>(expectedPage, expectedPerPage, tasks.size(), tasks);
+        final var expectedItems = expectedPagination.items().stream().map(TaskListOutput::from).toList();
+
+        final var expectedItemsCount = 2;
+
+        Mockito.when(taskGateway.findAll(Mockito.eq(aQuery)))
+                .thenReturn(expectedPagination);
+
+        final var actualResult = listTaskUseCase.execute(aQuery);
+
+        Assertions.assertEquals(expectedItemsCount, actualResult.items().size());
+        Assertions.assertEquals(expectedPage, actualResult.currentPage());
+        Assertions.assertEquals(expectedItems, actualResult.items());
+        Assertions.assertEquals(expectedPerPage, actualResult.perPage());
+        Assertions.assertEquals(tasks.size(), actualResult.total());
+
+        // when
+        // then
+    }
+
+}
