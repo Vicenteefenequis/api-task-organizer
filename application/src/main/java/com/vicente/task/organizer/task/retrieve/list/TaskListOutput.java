07cbe2cdd63cb53f0fb6b1294d21691eff692667
Vicente Junior <vicente19981@live.com>
2023-04-13 17:53:38 -0300

    feat: creat record task list output

---
+++ application/src/main/java/com/vicente/task/organizer/task/retrieve/list/TaskListOutput.java
@@ -0,0 +1,25 @@
+package com.vicente.task.organizer.task.retrieve.list;
+
+import com.vicente.task.organizer.task.Task;
+
+import java.time.Instant;
+
+public record TaskListOutput(
+        String id,
+        String name,
+        String description,
+        boolean isCompleted,
+        Instant dueDateAt,
+        Instant deletedAt
+) {
+    public static TaskListOutput from(final Task aTask) {
+        return new TaskListOutput(
+                aTask.getId().getValue(),
+                aTask.getName(),
+                aTask.getDescription(),
+                aTask.isCompleted(),
+                aTask.getDueDateAt(),
+                aTask.getDeletedAt()
+        );
+    }
+}
