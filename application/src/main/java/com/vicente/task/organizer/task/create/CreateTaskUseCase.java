d72f2d2c4e9183fb7b4edfbf556c94098f752ea7
Vicente Junior <vicente19981@live.com>
2023-04-12 22:29:01 -0300

    feat: create abstract usecase

---
+++ application/src/main/java/com/vicente/task/organizer/task/create/CreateTaskUseCase.java
@@ -0,0 +1,6 @@
+package com.vicente.task.organizer.task.create;
+
+import com.vicente.task.organizer.UseCase;
+
+public abstract class CreateTaskUseCase extends UseCase<CreateTaskCommand, CreateTaskOutput> {
+}
