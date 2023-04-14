69277597ffbaf1e06b5b7662ddc95b183cd782b2
Vicente Junior <vicente19981@live.com>
2023-04-14 00:00:02 -0300

    chore: adding spring boot configuration

---
+++ infrastructure/src/main/java/com/vicente/task/organizer/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.vicente.task.organizer.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.vicente.task.organizer")
+public class WebServerConfig {
+}
