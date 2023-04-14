7f2af60f248acdd92a242932c4c8a8f6aa0ba5a6
Vicente Junior <vicente19981@live.com>
2023-04-14 16:11:46 -0300

    feat: create task response

---
+++ infrastructure/src/main/java/com/vicente/task/organizer/task/models/TaskResponse.java
@@ -0,0 +1,17 @@
+package com.vicente.task.organizer.task.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.Instant;
+
+public record TaskResponse(
+        @JsonProperty("id") String id,
+        @JsonProperty("name") String name,
+        @JsonProperty("description") String description,
+        @JsonProperty("completed") boolean completed,
+        @JsonProperty("due_date_at") Instant dueDateAt,
+        @JsonProperty("created_at") Instant createdAt,
+        @JsonProperty("updated_at") Instant updatedAt,
+        @JsonProperty("deleted_at") Instant deletedAt
+) {
+}
