59fe3152971b4e838d1391adbab303f5d72093eb
Vicente Junior <vicente19981@live.com>
2023-04-14 16:11:38 -0300

    feat: create task mysql gateway

---
+++ infrastructure/src/main/java/com/vicente/task/organizer/task/TaskMySQLGateway.java
@@ -0,0 +1,36 @@
+package com.vicente.task.organizer.task;
+
+import com.vicente.task.organizer.pagination.Pagination;
+import com.vicente.task.organizer.pagination.SearchQuery;
+import com.vicente.task.organizer.task.persistence.TaskRepository;
+
+import java.util.Optional;
+
+public class TaskMySQLGateway implements TaskGateway {
+    private final TaskRepository taskRepository;
+
+    public TaskMySQLGateway(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+    @Override
+    public Task create(Task task) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(TaskID taskId) {
+
+    }
+    @Override
+    public Optional<Task> findById(TaskID taskId) {
+        return Optional.empty();
+    }
+    @Override
+    public Task update(Task task) {
+        return null;
+    }
+    @Override
+    public Pagination<Task> findAll(SearchQuery aQuery) {
+        return null;
+    }
+}
