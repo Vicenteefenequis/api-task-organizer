97908b557fa6778d4144df7bec63b6fda8f4dccc
Vicente Junior <vicente19981@live.com>
2023-04-16 22:49:19 -0300

    feat: adding cross origin

--- infrastructure/src/main/java/com/vicente/task/organizer/infrastructure/api/TaskAPI.java
+++ infrastructure/src/main/java/com/vicente/task/organizer/infrastructure/api/TaskAPI.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "tasks")
+@CrossOrigin(origins = "*")
 @Tag(name = "Tasks")
 public interface TaskAPI {
     @PostMapping(
